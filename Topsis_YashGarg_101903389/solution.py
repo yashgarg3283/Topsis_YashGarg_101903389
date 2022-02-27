9f463084eb2d818b4092c7168bd4e8a5462aa7a8
Yash Garg <54525203+yashgarg3283@users.noreply.github.com>
2022-02-27 22:36:14 +0530

    Update solution.py

--- Topsis_YashGarg_101903389/solution.py
+++ Topsis_YashGarg_101903389/solution.py
@@ -1,109 +1,127 @@
 import pandas as pd
-import math as m
+import os
 import sys
 
+name = "Topsis_Tewatia/Topsis_Tewatia"
+__version__ = "1.0.4"
+__author__ = 'Yash Kumar Tewatia'
+__credits__ = 'Thapar Institute of Engineering and Technology'
+
+
 def main():
-    if len(sys.argv)!=5 :
-         raise Exception("Number of parameter are incorrect")
-    if sys.argv[1].endswith(('.csv')):
-         pass
+    # Arguments not equal to 5
+    # print("Checking for Errors...\n")
+    if len(sys.argv) != 5:
+        print("ERROR : NUMBER OF PARAMETERS")
+        print("USAGE : python topsis.py inputfile.csv '1,1,1,1' '+,+,-,+' result.csv ")
+        exit(1)
+
+    # File Not Found error
+    elif not os.path.isfile(sys.argv[1]):
+        print(f"ERROR : {sys.argv[1]} Don't exist!!")
+        exit(1)
+
+    # File extension not csv
+    elif ".csv" != (os.path.splitext(sys.argv[1]))[1]:
+        print(f"ERROR : {sys.argv[1]} is not csv!!")
+        exit(1)
+
     else:
-         raise Exception("File type must be .csv")
-    fn=sys.argv[1]
-    
-    try:
-            df=pd.read_csv(fn)
-    except:
-            print("No such file is present")
-            sys.exit()
+        dataset, temp_dataset = pd.read_csv(
+            sys.argv[1]), pd.read_csv(sys.argv[1])
+        nCol = len(temp_dataset.columns.values)
 
-    wgts = sys.argv[2]
-    ipts = sys.argv[3]
-    rt=sys.argv[4]
-    
-    for i in  ipts:
-                
-            if i =='+' or i=='-' or ',':
-                pass
-            else:
-                raise Exception("Only positive and negative values are accepted")
-       
-    topsis(df,wgts,ipts,rt)
+        # less then 3 columns in input dataset
+        if nCol < 3:
+            print("ERROR : Input file have less then 3 columns")
+            exit(1)
 
-def topsis(data, Wgts, Imp,rt):
-    
+        # Handeling non-numeric value
+        for i in range(1, nCol):
+            pd.to_numeric(dataset.iloc[:, i], errors='coerce')
+            dataset.iloc[:, i].fillna(
+                (dataset.iloc[:, i].mean()), inplace=True)
 
-    dtst = data
-    X = dtst.iloc[:, 1:].values
-    X = X.astype(float)
+        # Handling errors of weighted and impact arrays
+        try:
+            weights = [int(i) for i in sys.argv[2].split(',')]
+        except:
+            print("ERROR : In weights array please check again")
+            exit(1)
+        impact = sys.argv[3].split(',')
+        for i in impact:
+            if not (i == '+' or i == '-'):
+                print("ERROR : In impact array please check again")
+                exit(1)
 
-    W = []
-    ipt = []
-    for i in Wgts:
-      if i != ',': W.append(float(i))
-    for i in Imp:
-      if i != ',': ipt.append(i)
+        # Checking number of column,weights and impacts is same or not
+        if nCol != len(weights)+1 or nCol != len(impact)+1:
+            print(
+                "ERROR : Number of weights, number of impacts and number of columns not same")
+            exit(1)
 
-    a, b, c, d = 0, 0, 0, 0
-    for i in range(len(X)):
-      a += X[i][0] * X[i][0]
-      b += X[i][1] * X[i][1]
-      c += X[i][2] * X[i][2]
-      d += X[i][3] * X[i][3]
-    a = float(m.sqrt(a))
-    b = float(m.sqrt(b))
-    c = float(m.sqrt(c))
-    d = float(m.sqrt(d))
+        if (".csv" != (os.path.splitext(sys.argv[4]))[1]):
+            print("ERROR : Output file extension is wrong")
+            exit(1)
+        if os.path.isfile(sys.argv[4]):
+            os.remove(sys.argv[4])
+        # print(" No error found\n\n Applying Topsis Algorithm...\n")
+        topsis_pipy(temp_dataset, dataset, nCol, weights, impact)
 
-    for i in range(len(X)):
-      for j in range(len(X[0])):
-        if j == 0: X[i][j] /= a
-        elif j == 1: X[i][j] /= b
-        elif j == 2: X[i][j] /= c
-        elif j == 3: X[i][j] /= d
 
-    for i in range(len(X)):
-      for j in range(len(X[0])):
-        if j == 0: X[i][j] *= W[0]
-        elif j == 1: X[i][j] *= W[1]
-        elif j == 2: X[i][j] *= W[2]
-        elif j == 3: X[i][j] *= W[3]
+def Normalize(temp_dataset, nCol, weights):
+    # normalizing the array
+    # print(" Normalizing the DataSet...\n")
+    for i in range(1, nCol):
+        temp = 0
+        for j in range(len(temp_dataset)):
+            temp = temp + temp_dataset.iloc[j, i]**2
+        temp = temp**0.5
+        for j in range(len(temp_dataset)):
+            temp_dataset.iat[j, i] = (
+                temp_dataset.iloc[j, i] / temp)*weights[i-1]
+    return temp_dataset
 
-    vj_p = []
-    vj_m = []
-    for j in range(len(X[0])):
-      mn=1000
-      mx=-1
-      for i in range(len(X)):
-        mn = min(mn, X[i][j])
-        mx = max(mx, X[i][j])
-      if (ipt[j] == '+'):
-        vj_p.append(mx)
-        vj_m.append(mn)
-      else:
-        vj_p.append(mn)
-        vj_m.append(mx)
 
-    Sj_plus = []
-    Sj_minus = []
-    for i in range(len(X)):
-      sum1, sum2 = 0, 0
-      for j in range(len(X[0])):
-        sum1 += (X[i][j] - vj_p[j]) ** 2
-        sum2 += (X[i][j] - vj_m[j]) ** 2
-      Sj_plus.append(m.sqrt(sum1))
-      Sj_minus.append(m.sqrt(sum2))
+def Calc_Values(temp_dataset, nCol, impact):
+    # print(" Calculating Positive and Negative values...\n")
+    p_sln = (temp_dataset.max().values)[1:]
+    n_sln = (temp_dataset.min().values)[1:]
+    for i in range(1, nCol):
+        if impact[i-1] == '-':
+            p_sln[i-1], n_sln[i-1] = n_sln[i-1], p_sln[i-1]
+    return p_sln, n_sln
 
-    tp_s = []
-    for i in range(len(Sj_plus)):
-      tp_s.append(Sj_minus[i] / (Sj_plus[i] + Sj_minus[i]))
 
-    dtst["Topsis Score"] = tp_s
-    dtst["Rank"] = dtst["Topsis Score"].rank(ascending= False) 
-    dtst.sort_values("Topsis Score", inplace = True)
-    dtst = dtst.sort_index(axis=0)
-    dtst.to_csv(rt,index=False)
-    # print(dtst)
+def topsis_pipy(temp_dataset, dataset, nCol, weights, impact):
+    # normalizing the array
+    temp_dataset = Normalize(temp_dataset, nCol, weights)
+
+    # Calculating positive and negative values
+    p_sln, n_sln = Calc_Values(temp_dataset, nCol, impact)
+
+    # calculating topsis score
+    # print(" Generating Score and Rank...\n")
+    score = []
+    for i in range(len(temp_dataset)):
+        temp_p, temp_n = 0, 0
+        for j in range(1, nCol):
+            temp_p = temp_p + (p_sln[j-1] - temp_dataset.iloc[i, j])**2
+            temp_n = temp_n + (n_sln[j-1] - temp_dataset.iloc[i, j])**2
+        temp_p, temp_n = temp_p**0.5, temp_n**0.5
+        score.append(temp_n/(temp_p + temp_n))
+    dataset['Topsis Score'] = score
+
+    # calculating the rank according to topsis score
+    dataset['Rank'] = (dataset['Topsis Score'].rank(
+        method='max', ascending=False))
+    dataset = dataset.astype({"Rank": int})
+
+    # Writing the csv
+    # print(" Writing Result to CSV...\n")
+    dataset.to_csv(sys.argv[4], index=False)
+    # print(" Successfully Terminated")
+
 
 if __name__ == "__main__":
     main()
