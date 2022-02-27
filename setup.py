28f66d1ca7372b86122d992a47c652cb46a014a7
Yash Garg <54525203+yashgarg3283@users.noreply.github.com>
2022-02-27 22:44:10 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Yash Garg',                   # Type in your name
   author_email = 'ygarg_be19@thapar.edu',      # Type in your E-Mail
   url = 'https://github.com/yashgarg3283/Topsis_YashGarg_101903389',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/yashgarg3283/Topsis_YashGarg_101903389/archive/refs/tags/0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/yashgarg3283/Topsis_YashGarg_101903389/archive/refs/tags/0.2.tar.gz',    # I explain this later on
   keywords = ['Topsis','Yash'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pandas',
